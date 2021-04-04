1b2c2bdf02a86eb81672bed409d3f3408a1383b1
ivanbarto <ivanbartolelli@gmail.com>
2021-04-04 14:07:40 -0300

    tp4 terminado

---
+++ TP04-Bartolelli-Curto.py
@@ -0,0 +1,25 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+frameRate = cap.get(cv2.CAP_PROP_FPS)
+
+fourcc = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
+
+out = cv2.VideoWriter('res/output.avi', fourcc, frameRate, (int(float(width)), int(float(height))))
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret is True:
+        out.write(frame)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
