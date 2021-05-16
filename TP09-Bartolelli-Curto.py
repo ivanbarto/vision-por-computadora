3ce97c50d03ff236ceed712c1b4585bc11110f57
fedecurto98 <fedecurto1@gmail.com>
2021-05-16 13:02:25 -0300

    TP09 Finalizado

--- TP09-Bartolelli-Curto.py
+++ TP09-Bartolelli-Curto.py
@@ -3,18 +3,19 @@
 import cv2
 import numpy as np
 
-# Turn on Laptop's webcam
-cap = cv2.VideoCapture(0)
+img = cv2.imread("res/bombones.jpeg")
+selected_pts = []
 
 def on_EVENT_LBUTTONDOWN(event, x, y, flags, param):
-    global selected_pts, background_img
+    global selected_pts, img
 
     if event == cv2.EVENT_LBUTTONDOWN:
         selected_pts.append([x, y])
-        cv2.circle(background_img, (x, y), 1, (255, 0, 0), thickness=-1)
+        cv2.circle(img, (x, y), 1, (255, 0, 0), thickness=-1)
 
 
-def select_points(image, points_num):
+def select_points(image):
+    # Los puntos se deben seleccionar de izquierda a derecha y de arriba hacia abajo
     global selected_pts
 
     cv2.namedWindow("image")
@@ -24,34 +25,30 @@ def select_points(image, points_num):
         cv2.imshow("image", image)
         k = cv2.waitKey(1)
 
-        if k == 27 or len(selected_pts) == points_num:
+        # Se evalua que se marquen 4 puntos
+        if k == 27 or len(selected_pts) == 4:
             break
 
     cv2.destroyAllWindows()
 
     return np.array(selected_pts, dtype=np.float32)
 
-src_pts = select_points(background_img, 4)
+src_pts = select_points(img)
 
 
 while True:
 
-    ret, frame = cap.read()
-
-    # Locate points of the documents or object which you want to transform
-    pts1 = np.float32([[0, 260], [640, 260], [0, 400], [640, 400]])
-    pts2 = np.float32([[0, 0], [400, 0], [0, 640], [400, 640]])
+    # Dimensiones de destino
+    dst_dimension = np.float32([[0, 0], [800, 0], [0, 600], [800, 600]])
 
     # Apply Perspective Transform Algorithm
-    matrix = cv2.getPerspectiveTransform(pts1, pts2)
-    result = cv2.warpPerspective(frame, matrix, (500, 600))
+    matrix = cv2.getPerspectiveTransform(src_pts, dst_dimension)
+    result = cv2.warpPerspective(img, matrix, (800, 600))
     # Wrap the transformed image
 
-    cv2.imshow('frame', frame)  # Inital Capture
-    cv2.imshow('frame1', result)  # Transformed Capture
+    cv2.imshow('perspective transform', result)  # Transformed Capture
 
     if cv2.waitKey(24) == 27:
         break
 
-cap.release()
 cv2.destroyAllWindows()
