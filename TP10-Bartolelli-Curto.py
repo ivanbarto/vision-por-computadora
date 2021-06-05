7071be5c6134d5e455e3c702ed230a74f281096d
ivanbarto <ivanbartolelli@gmail.com>
2021-06-05 11:06:36 -0300

    tp 10 cambios

--- TP10-Bartolelli-Curto.py
+++ TP10-Bartolelli-Curto.py
@@ -5,6 +5,7 @@ import numpy as np
 
 img = cv2.imread("res/fachada_habitacion.png")
 selected_pts = []
+relacion_aspecto = 0
 
 
 def on_EVENT_LBUTTONDOWN(event, x, y, flags, param):
@@ -16,6 +17,7 @@ def on_EVENT_LBUTTONDOWN(event, x, y, flags, param):
 
 
 def select_points(image, ancho_marco, alto_marco):
+    relacion_aspecto = ancho_marco/alto_marco
     # Los puntos se deben seleccionar de izquierda a derecha y de arriba hacia abajo
     global selected_pts
 
@@ -45,22 +47,23 @@ def select_points(image, ancho_marco, alto_marco):
     return np.array(selected_pts, dtype=np.float32)
 
 
-src_pts = select_points(img, 76, 206)
+src_pts = select_points(img, 76, 206) #pasar ancho y alto
 
-#
-# while True:
-#
-#     # Dimensiones de destino
-#     dst_dimension = np.float32([[0, 0], [800, 0], [0, 600], [800, 600]])
-#
-#     # Apply Perspective Transform Algorithm
-#     matrix = cv2.getPerspectiveTransform(src_pts, dst_dimension)
-#     result = cv2.warpPerspective(img, matrix, (800, 600))
-#     # Wrap the transformed image
-#
-#     cv2.imshow('perspective transform', result)  # Transformed Capture
-#
-#     if cv2.waitKey(24) == 27:
-#         break
+
+while True:
+
+    # Dimensiones de destino
+    dst_dimension = np.float32([[0, 0], [img.shape[1], 0], [0, img.shape[0]], [img.shape[1], img.shape[0]]])
+
+    # Apply Perspective Transform Algorithm
+    matrix = cv2.getPerspectiveTransform(src_pts, dst_dimension)
+    result = cv2.warpPerspective(img, matrix, (img.shape[1], img.shape[0]))
+    # Wrap the transformed image
+
+    cv2.imshow('perspective transform', result)  # Transformed Capture
+
+    if cv2.waitKey(24) == 27:
+        break
+
 
 cv2.destroyAllWindows()
