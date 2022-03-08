1d31a0fca45cc9a87f0233ea3c0cf456f7fc1946
ivanbarto <ivanbartolelli@gmail.com>
2022-03-08 00:38:27 -0300

    agregado TP detector de barbijos

--- TP06-Bartolelli-Curto.py
+++ TP06-Bartolelli-Curto.py
@@ -1,19 +1,21 @@
 import cv2
 import numpy as np
 
+pi = 22 / 7
+rad_conversor = pi / 180
+
 
 def euclidean_transformation(img, angle, tx, ty):
     (h, w) = (img.shape[0], img.shape[1])
-    transformation_matrix = np.float32([[np.cos(angle), np.sin(angle), tx],
-                                        [-np.sin(angle), np.cos(angle), ty]])
+    transformation_matrix = np.float32([[np.cos(angle*rad_conversor), np.sin(angle*rad_conversor), tx],
+                                        [-np.sin(angle*rad_conversor), np.cos(angle*rad_conversor), ty]])
     shifted = cv2.warpAffine(img, transformation_matrix, (w, h))
 
     return shifted
 
 
 img = cv2.imread("res/hondaVFR.png")
-cv2.imwrite("res/trans_euclideana.png", euclidean_transformation(img, 12, 120, -120))
+cv2.imwrite("res/trans_euclideana.png", euclidean_transformation(img, 45, 0, 0))
 transformed_img = cv2.imread("res/trans_euclideana.png")
 cv2.imshow("transformacion euclideana", transformed_img)
 cv2.waitKey(0)
-
