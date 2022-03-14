4690d4ab46f4da231c44bfa16b4e1f4b3a128716
ivanbarto <ivanbartolelli@gmail.com>
2022-03-14 19:11:14 -0300

    minor fix in TP3

--- TP03-Bartolelli-Curto.py
+++ TP03-Bartolelli-Curto.py
@@ -10,15 +10,23 @@ import cv2
 #     sys.exit(0)
 #
 # cap = cv2.VideoCapture(filename)
+MILLISECOND = 1000
 cap = cv2.VideoCapture('res/motogp_crash.mp4')
-fps = int(float(cap.get(cv2.CAP_PROP_FPS)))
+(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+
+if int(major_ver) < 3:
+    fps = int(float(cap.get(cv2.CAP_PROP_FPS)))
+else:
+    fps = int(float(cap.get(cv2.CAP_PROP_FPS)))
+
+print(fps)
 
 while cap.isOpened():
     ret, frame = cap.read()
     if ret is True:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         cv2.imshow('frame', gray)
-        if cv2.waitKey(fps) & 0xFF == ord('q'):
+        if cv2.waitKey(int(MILLISECOND/fps)) & 0xFF == ord('q'):
             break
     else:
         break
